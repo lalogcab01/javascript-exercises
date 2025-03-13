71fce36bb9f9337ef1342d9e978973a22198d65a
eduardo <152088936+lalogcab01@users.noreply.github.com>
2025-03-13 11:43:05 -0600

    Solutions to 05_SUMALL

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+function gaussSum(num) {
+  return (num * (num + 1))/2;
+}
+function isPositiveInteger(num) {
+  if (Number.isInteger(num) && (num > 0)) {
+    return true;
+  }
+  return false;
+}
 
+const sumAll = function(num1, num2) {
+  if (isPositiveInteger(num1) && isPositiveInteger(num2)) {
+    min = Math.min(num1, num2);
+    max = Math.max(num1, num2);
+    return gaussSum(max) - gaussSum(min - 1);
+  }
+  return 'ERROR';
 };
 
 // Do not edit below this line
