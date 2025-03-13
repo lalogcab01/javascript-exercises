43817f0e6f3f24b403378d6b29f0522320167470
eduardo <152088936+lalogcab01@users.noreply.github.com>
2025-03-13 10:54:20 -0600

    Solution to 04_REMOVEFROMARRAY

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
 const removeFromArray = function() {
+  array = arguments[0];
+
+  for (let i = 1; i < arguments.length; i++) {
+
+    value = arguments[i];
+
+    let j = 0;
+    while (j < array.length) {
+      if (array[j] === value) {
+        // The following lines are sugested in https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array-in-javascript
+        // if (j > -1) {
+        //   array.splice(j, 1);
+        // }
+        array.splice(j, 1);
+      } else {
+        j++;
+      }
+    }
+
+  }
+
+  return array;
 };
 
 // Do not edit below this line
