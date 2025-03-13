842383a0b7deadedffe8e4df82fc990af0daf195
eduardo <152088936+lalogcab01@users.noreply.github.com>
2025-03-13 09:28:24 -0600

    Solution to 02_REPEATSTRING

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(message, repetitions) {
+  if (repetitions < 0) {
+    return 'ERROR';
+  }
 
+  let outString = "";
+  
+  i = 0;
+  while (i<repetitions) {
+    outString = outString + message;
+    i++;
+  }
+
+  return outString;
 };
 
 // Do not edit below this line
